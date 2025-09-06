5d4ed3c56b21361a157cc6153ccdd4432c6aa584
easybandz777 <honestroofingcompany.gmail.com>
2025-09-06 18:44:42 -0400

    Fix absolute import in homepage - use relative path for Button component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import { Button } from '../components/ui/button'
 import { 
   Package, 
   GraduationCap, 
